24e496280057f5cfbc2604d05e6832949a2c6ac8
chenxf <chenxf1@pingpongx.com>
2019-07-29 14:33:46 +0800

    recover

--- src/question.js
+++ src/question.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
     name: "git",
-    type: "expand",
+    type: "list",
     message: "请选择您要进行的git操作：",
     choices: [
-      { key: "A", name: "提交代码", value: "push" },
-      { key: "B", name: "切换分支", value: "checkout" },
-      { key: "C", name: "新建feature分支", value: "feature" },
-      { key: "D", name: "新建release分支", value: "release" },
-      { key: "E", name: "自动tag", value: "tag" }
+      { name: "提交代码", value: "push" },
+      { name: "切换分支", value: "checkout" },
+      { name: "新建feature分支", value: "feature" },
+      { name: "新建release分支", value: "release" },
+      { name: "自动tag", value: "tag" }
     ],
     filter: function(val) {
       // 使用filter将回答变为小写
